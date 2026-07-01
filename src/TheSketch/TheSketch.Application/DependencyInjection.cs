914a05e7e23e1d326c02708d1cfed21f07fbdd89
Savchuk Viacheslav <viacheslav.if4@gmail.com>
2026-07-01 18:58:55 +0300

    Add DI setup and service registration to Application layer
    Introduced DependencyInjection static class with AddApplication extension method for registering application services. Registered IArticleService with ArticleService implementation.

---
+++ src/TheSketch/TheSketch.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TheSketch.Application.Interfaces.Services;
+using TheSketch.Application.Services;
+
+namespace TheSketch.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IArticleService, ArticleService>();
+
+        return services;
+    }
+}
