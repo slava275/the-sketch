b639c3899f53bef299ed3a0e319baac1b85a000c
Savchuk Viacheslav <viacheslav.if4@gmail.com>
2026-07-01 17:24:41 +0300

    Initial domain model and project setup
    Added .gitignore for Visual Studio/.NET. Introduced Article and ArticleBlock domain entities with supporting enums and block types. Created TheSketch.Domain project targeting .NET 10.0 and added solution file.

---
+++ src/TheSketch/TheSketch.Domain/Entities/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSketch.Domain.Entities;
+
+public class Article
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Slug { get; set; } = null!;
+    public string? Description { get; set; }
+    public string? CoverImageUrl { get; set; }
+    public byte TimeToRead { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public ArticleCategory Category { get; set; }
+    public List<string> Tags { get; set; } = new ();
+    public List<ArticleBlock> Blocks { get; set; } = new();
+}
+
+public enum ArticleCategory
+{
+    World,
+    Ukraine
+}
---
+++ src/TheSketch/TheSketch.Domain/Entities/ArticleBlock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSketch.Domain.Entities;
+
+public abstract class ArticleBlock
+{
+    public string Type { get; set; } = null!;
+}
+
+public class ParagraphBlock : ArticleBlock
+{
+    public ParagraphBlock() => Type = "paragraph";
+
+    public string Text { get; set; } = null!;
+}
+
+public class ImageBlock : ArticleBlock
+{
+    public ImageBlock() => Type = "image-wide";
+    public string Url { get; set; } = null!;
+    public string? Caption { get; set; }
+}
+
+public class TitleBlock : ArticleBlock
+{
+    public TitleBlock() => Type = "title";
+    public string Text { get; set; } = null!;
+}
+
+public class QuoteBlock : ArticleBlock
+{
+    public QuoteBlock() => Type = "quote";
+    public string Text { get; set; } = null!;
+    public string? Author { get; set; }
+}
+
+public class SubtitleBlock : ArticleBlock
+{
+    public SubtitleBlock() => Type = "subtitle";
+    public string Text { get; set; } = null!;
+}
