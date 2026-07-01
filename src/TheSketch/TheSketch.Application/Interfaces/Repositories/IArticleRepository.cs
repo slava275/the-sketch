c04108f966c36b3ba718c294eb2fd7e50ede5701
Savchuk Viacheslav <viacheslav.if4@gmail.com>
2026-07-01 17:40:31 +0300

    Add Application project and IArticleRepository interface
    Added TheSketch.Application project targeting .NET 10.0 with reference to TheSketch.Domain. Introduced IArticleRepository interface for async CRUD and query operations on Article entities.

---
+++ src/TheSketch/TheSketch.Application/Interfaces/Repositories/IArticleRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSketch.Domain.Entities;
+
+namespace TheSketch.Application.Interfaces.Repositories;
+
+public interface IArticleRepository
+{
+    Task AddAsync(Article article);
+    void Update(Article article);
+    void Delete(Article article);
+
+    Task<Article?> GetByIdAsync(Guid id);
+    Task<Article?> GetBySlugAsync(string slug);
+    Task<IEnumerable<Article>> GetAllAsync(int page = 1, int pageSize = 10);
+    Task<IEnumerable<Article>> GetByCategoryAsync(ArticleCategory category, int page = 1, int pageSize = 10);
+    Task<IEnumerable<Article>> GetByTagAsync(string tag, int page = 1, int pageSize = 10);
+
+    Task<IEnumerable<Article>> SearchByTitleAsync(string searchTerm);
+    Task<IEnumerable<Article>> GetByDateRangeAsync(DateTime startDate, DateTime endDate);
+
+    Task<bool> ExistsAsync(Guid id);
+}
